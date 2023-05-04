d79e15cf07df2cf3213bc585dbb352e1c67bbf99
Markus <ModernFuzzBall2@hotmail.com>
2023-05-04 00:38:42 -0400

    Added google analytics

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import VueGtag from 'vue-gtag'
 
 createApp(App)
 .use(router)
+.use(VueGtag, {
+    config: { id: "G-9Z3MH28VEJ" }
+})
 .mount('#app')
