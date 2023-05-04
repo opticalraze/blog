638bb32023a20363d274f81b1fb646de37c7683d
Markus <ModernFuzzBall2@hotmail.com>
2023-05-04 00:43:31 -0400

    Update Post.vue

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,35 +1,11 @@
 <script>
 import { defineComponent } from "vue";
 
-import showdown from "showdown";
-
 export default defineComponent({
     async setup() {
         const blogs = await fetch("/blogs.json").then(res => res.json());
-
-        //var showdown  = require('showdown'),
-        const converter = new showdown.Converter();
-        const text = `1. First ordered list item
-2. Another item
-⋅⋅* Unordered sub-list. 
-1. Actual numbers don't matter, just that it's a number
-⋅⋅1. Ordered sub-list
-4. And another item.
-
-⋅⋅⋅You can have properly indented paragraphs within list items. Notice the blank line above, and the leading spaces (at least one, but we'll use three here to also align the raw Markdown).
-
-⋅⋅⋅To have a line break without a paragraph, you will need to use two trailing spaces.⋅⋅
-⋅⋅⋅Note that this line is separate, but within the same paragraph.⋅⋅
-⋅⋅⋅(This is contrary to the typical GFM line break behaviour, where trailing spaces are not required.)
-
-* Unordered list can use asterisks
-- Or minuses
-+ Or pluses`;
-        const html = converter.makeHtml(text);
-
         return {
-            blogs,
-            html
+            blogs
         };
     },
     mounted() {
@@ -52,7 +28,7 @@ export default defineComponent({
         <div class="text-center">
             <img :src="blogs[$route.params.id].image" alt="" class="w-1/3 inline-block">
         </div>
-        <div class="markdown" v-html="html"></div>
+        
     </div>
     
 </template>
